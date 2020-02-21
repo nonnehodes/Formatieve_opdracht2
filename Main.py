902449dea4620b20725e5ac19320f0888cb9ab9e
nonnehodes <nonne.hodes@student.hu.nl>
2020-02-21 21:49:03 +0100

    opdrachten 1 en 2 af

--- Main.py
+++ Main.py
@@ -1,16 +1,24 @@
+import pymongo
+client = pymongo.MongoClient("mongodb://localhost:27017/")
+webshop_databases = client['huwebshop']
+products_database = webshop_databases['products']
+sessions_database = webshop_databases['sessions']
+profiles_database = webshop_databases['profiles']
+#   Opdracht 1
+print(' Opdracht 1:')
+producten = products_database.find({},{'name':1, 'price':1})
+print('Naam v/h eerste product in de DB: {}'.format(producten[0]['name']))
+print('Prijs v/h eerste product in de DB: {}'.format(producten[0]['price']['selling_price']))
+
+#   Opdracht 2
+print('\n Opdracht 2:')
+producten_2 = products_database.find({ "name": { "$gt": "R" } }, {'name':1})
+print('Naam v/h eerste product met een r: {}'.format(producten_2[0]['name']))
+
+
+#   Opdracht 3
+
 '''
 Bron 1: https://www.w3schools.com/python/python_mongodb_find.asp
 
 '''
-
-import pymongo
-
-client = pymongo.MongoClient("mongodb://localhost:27017/")
-database = client["huwebshop"]
-colom = database["products"]
-
-
-#   Opdracht 2
-myquery = { "name": { "$gt": "R" } }
-mydoc = colom.find(myquery)
-
