5912c08149649de935eeb6e99010c0135bf3fe50
MoosHU1 <moos.boot@student.hu.nl>
2020-02-22 12:59:57 +0100

    opdracht af

--- Main.py
+++ Main.py
@@ -7,10 +7,45 @@ import pymongo
 
 client = pymongo.MongoClient("mongodb://localhost:27017/")
 database = client["huwebshop"]
-colom = database["products"]
+
+
+
+#   Opdracht 1
+def opdracht_1():
+    colom_product = database["products"]
+
+    results = colom_product.find()
+    for result in results:
+        print(result["name"], (result["price"])['selling_price'])
+        break
+
 
 
 #   Opdracht 2
-myquery = { "name": { "$gt": "R" } }
-mydoc = colom.find(myquery)
+def opdracht_2():
+    colom_product = database["products"]
 
+    myquery = {'name':{"$regex": "^R" }}
+    results= colom_product.find(myquery)
+    for result in results:
+        print(result['name'])
+        break
+
+def opdracht_3():#  Deze functie duurt even
+    colom_product = database["products"]
+    results = colom_product.find()
+
+    total = 0
+    loop = 0
+
+    for result in results:
+        try:
+            total += ((result["price"])["selling_price"])
+        except:
+            continue
+        else:
+            loop +=1
+
+    print(total/loop)
+
+opdracht_3()
