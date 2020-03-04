a93b914797bd6e767cc84fa2cbc460eb89a63747
MoosHU1 <moos.boot@student.hu.nl>
2020-03-04 17:03:49 +0100

    data van mongodb naar postgresql werkt

--- Formatieve_opdracht2b.py
+++ Formatieve_opdracht2b.py
@@ -6,30 +6,40 @@ Bron 1: https://pynative.com/python-postgresql-insert-update-delete-table-data-t
 import pymongo
 import psycopg2  # Module om met PostgreSQL te communiceren
 
-client = pymongo.MongoClient("mongodb://localhost:27017/")
-database = client["huwebshop"]
 
 
-try:
-    connection = psycopg2.connect("dbname=voordeelshop user=postgres password=ZET HIER JE WW NEER")   # Verbind met Postgres database
-    cursor = connection.cursor()
 
-    postgres_insert_query = """ INSERT INTO product VALUES(%s,%s,%s,%s)"""
-    record_to_insert = ('123', 'Man', 'Shirt', '1')
-    cursor.execute(postgres_insert_query, record_to_insert)
+def get_products_mongo():
+    client = pymongo.MongoClient("mongodb://localhost:27017/")
+    database = client["huwebshop"]
 
-    connection.commit()
-    count = cursor.rowcount
-    print(count, "Record inserted successfully into mobile table")
+    colom_product = database["products"]
 
-except (Exception, psycopg2.Error) as error:
-    if(connection):
-        print("Failed to insert record into mobile table", error)
+    products = colom_product.find({"gender": "Unisex"}).limit(20)
+    for i in products:
+        insert_into_postgres((i["_id"], i["gender"], i["name"], i["price"]["selling_price"]))
 
-finally:
-    #   closing database connection.
-    if(connection):
-        cursor.close()
-        connection.close()
-        print("PostgreSQL connection is closed")
+
+
+
+
+def insert_into_postgres(values):
+    try:
+        connection = psycopg2.connect("dbname=voordeelshop user=postgres password=WACHTWOORD")
+        cursor = connection.cursor()
+
+        postgres_insert_query = """ INSERT INTO product VALUES(%s,%s,%s,%s)"""
+        record_to_insert = values
+        cursor.execute(postgres_insert_query, record_to_insert)
+
+        connection.commit()
+        count = cursor.rowcount
+        print(count, "Record inserted successfully into mobile table")
+
+    except (Exception, psycopg2.Error) as error:
+        if(connection):
+            print("Failed to insert record into mobile table", error)
+
+
+get_products_mongo()
 
