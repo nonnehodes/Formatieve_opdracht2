ddc9d498f9d076e9304966f86007b06d73af3b86
MoosHU1 <moos.boot@student.hu.nl>
2020-03-04 17:16:21 +0100

    opdracht 2

--- Formatieve_opdracht2b.py
+++ Formatieve_opdracht2b.py
@@ -1,11 +1,13 @@
 '''
 Bron 1: https://pynative.com/python-postgresql-insert-update-delete-table-data-to-perform-crud-operations/#Python_PostgreSQL_INSERT_into_database_Table
 
+LET OP
+Het wachtwoord van je PostgreSQL moet je zelf invullen, anders werkt het niet
+Zoek naar de plekken waar je een wachtwoord in moet vullen met ctrl+f password
 '''
 
 import pymongo
 import psycopg2  # Module om met PostgreSQL te communiceren
-
 
 
 
@@ -20,12 +22,9 @@ def get_products_mongo():
         insert_into_postgres((i["_id"], i["gender"], i["name"], i["price"]["selling_price"]))
 
 
-
-
-
 def insert_into_postgres(values):
     try:
-        connection = psycopg2.connect("dbname=voordeelshop user=postgres password=WACHTWOORD")
+        connection = psycopg2.connect("dbname=voordeelshop user=postgres password=")
         cursor = connection.cursor()
 
         postgres_insert_query = """ INSERT INTO product VALUES(%s,%s,%s,%s)"""
@@ -41,5 +40,31 @@ def insert_into_postgres(values):
             print("Failed to insert record into mobile table", error)
 
 
-get_products_mongo()
+#get_products_mongo()
 
+
+def opdracht_2():
+    try:
+        connection = psycopg2.connect("dbname=voordeelshop user=postgres password=")
+        cursor = connection.cursor()
+
+        cursor = connection.cursor()
+        postgreSQL_select_Query = "select Price from Product"
+
+        cursor.execute(postgreSQL_select_Query)
+        price_records = cursor.fetchall()
+
+        total = 0
+        b = 0
+        for row in price_records:
+            total += row[0]
+            b += 1
+        print(total/b)
+
+
+    except (Exception, psycopg2.Error) as error:
+        if (connection):
+            print("Failed to insert record into mobile table", error)
+
+
+opdracht_2()
